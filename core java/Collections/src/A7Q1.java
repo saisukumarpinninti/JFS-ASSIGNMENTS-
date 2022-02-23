ac64c9e464db947ec05b2d8d7b43a9f59940a51f
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-02-23 16:15:48 +0530

    updated Collections assingnments

--- core java/Collections/src/A7Q1.java
+++ core java/Collections/src/A7Q1.java
@@ -4,25 +4,20 @@ import java.util.Map.Entry;
 class Details implements Comparable<Details>{
     private String name;
     private String email;
-    private String gender;
-
-    public Details(String name, String email, String gender) {
+    private Gender gender;
+    public Details(String name, String email, Gender gender) {
         super();
         this.name = name;
         this.email = email;
         this.gender = gender;
     }
-
     public String getName() {
         return name;
     }
-
-
     @Override
     public String toString() {
         return "Name:"+name+" Email:"+email+" Gender:"+gender;
     }
-
     @Override
     public int compareTo(Details o) {
         return this.getName().compareTo(o.getName());
@@ -32,15 +27,12 @@ class Details implements Comparable<Details>{
 
 class phoneNumber implements Comparable<phoneNumber>{
     private long number;
-
     public phoneNumber(long number) {
         super();
         this.number = number;
     }
-
     public String toString() {
         return String.valueOf(number);
-
     }
 
     @Override
@@ -51,33 +43,34 @@ class phoneNumber implements Comparable<phoneNumber>{
 
 
 }
-public class Q1 {
+
+public class A7Q1 {
 
     public static void main(String[] args) {
-        Details D1 = new Details("sukumar", "itsnotmymail@gmail.com", "Male");
-        Details D2 = new Details("name2", "name2@gmail.com", "Male");
-        Details D3 = new Details("name2", "name2@gmail.com", "Male");
+
+        // adding details and phone numbers
+        Details D1 = new Details("sukumar", "itsnotmymail@gmail.com", Gender.MALE);
+        Details D2 = new Details("name2", "name2@gmail.com", Gender.MALE);
+        Details D3 = new Details("name3", "name3@gmail.com", Gender.FEMALE);
         phoneNumber P1 = new phoneNumber(9010203540l);
         phoneNumber P2 = new phoneNumber(9234667890l);
         phoneNumber P3 = new phoneNumber(9643534545l);
-
+        // putting them into a treemap
         SortedMap<phoneNumber, Details> phonebook = new TreeMap<>(Comparator.reverseOrder());
         phonebook.put(P1,D1);
         phonebook.put(P2,D2);
         phonebook.put(P3,D3);
-
+        //printing keys
         Set<Entry<phoneNumber,Details>> entrySet = phonebook.entrySet();
         System.out.println("printing all keys ");
-
         int i = 0;
         for (Entry<phoneNumber, Details> currentEntry :entrySet) {
             System.out.println("Key at " + i + ":"+ currentEntry.getKey());
             i++;
         }
         System.out.println("\n");
-
+        //printing Values
         System.out.println("printing all Values ");
-
         int i2=0;
         for (Entry<phoneNumber,Details> currentEntry :entrySet) {
             System.out.println("Value at " + i2 + ":"+ currentEntry.getValue());
@@ -86,11 +79,8 @@ public class Q1 {
         System.out.println("\n");
 
         System.out.println("printing all keys  & their values");
-
         for (Entry<phoneNumber, Details> entry : phonebook.entrySet()) {
-            System.out.println("Key = " + entry.getKey() +
-                    ", Value = " + entry.getValue());
-
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
         }
         System.out.println("\n");
 
--- core java/Collections/src/Q3.java
+++ core java/Collections/src/Q3.java
@@ -1,6 +1,8 @@
 import javax.lang.model.element.Element;
 import java.util.*;
 
+
+
 class Employee implements Comparable<Object> {
     Scanner sc = new Scanner(System.in);
     public String id;
@@ -8,63 +10,134 @@ class Employee implements Comparable<Object> {
     public long salary;
     public String department;
 
-    public Employee() {
-        this.id = getId();
-        this.name = getName();
-        this.salary = getSalary();
-        this.department = getDepartment();
+    public Employee(String id, String name, long salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
     }
-
     //get id of employee
     public String getId() {
-        System.out.println("Enter employee id ");
-        String id = sc.next();
+        this.id=id;
         return id;
     }
 
     //get name of employee
     public String getName() {
-        System.out.println("Enter employee name");
-        String name = sc.next();
+        this.name = name;
         return name;
     }
 
     //get salary of employee
     public long getSalary() {
-        System.out.println("Enter employee salary ");
-        long salary = sc.nextLong();
+        this.salary = salary;
         return salary;
     }
 
     // get department
     public String getDepartment() {
-        System.out.println("Enter employee department ");
-        String department = sc.next();
+        this.salary = salary;
         return department;
     }
 
-    public static void displayDetails(Employee employee) {
-        {
-            System.out.println(employee.id);
-            System.out.println(employee.name);
-            System.out.println(employee.name);
-            System.out.println(employee.department);
-        }
+    public String toString(){
+        return "Employee [Id="+getId()+",name ="+getName()+",Department="+getDepartment()+",Salary"+getSalary()+"]";
     }
-
     @Override
     public int compareTo(Object o) {
         return 1;
     }
+
 }
+
+class idComparator implements Comparator<Employee>
+{
+    @Override
+    public int compare(Employee s1, Employee s2)
+    {
+        return s1.getName().compareTo(s2.getName());
+    }
+}
+class nameComparator implements Comparator<Employee>
+{
+    @Override
+    public int compare(Employee s1, Employee s2)
+    {
+        return s1.getName().compareTo(s2.getName());
+    }
+}
+class DepartmentComparator implements Comparator<Employee>
+{
+    @Override
+    public int compare(Employee s1, Employee s2)
+    {
+        return s1.getDepartment().compareTo(s2.getDepartment());
+    }
+}
+class SalaryComparator implements Comparator<Employee>
+{
+    @Override
+    public int compare(Employee s1, Employee s2)
+    {
+        return (int) (s1.getSalary()-(s2.getSalary()));
+    }
+}
+
 public class Q3 {
     public static void main(String[] args) {
-        Set<Employee> employees1 = new TreeSet<>();
-        employees1.add(new Employee());
-        employees1.add(new Employee());
-        for( Employee employee : employees1 ){
-            Employee.displayDetails(employee);
+        Employee emp0 = new Employee("100", "shyam", 88000l, "mechanical");
+        Employee emp1 = new Employee("101", "sukumar", 80000l, "Cse");
+        Employee emp2 = new Employee("102", "ganesh", 40000l, "electrical");
+        System.out.println("enter Your Option : "+"\n"+" \" 1 .\" for Sort by name "+"\n"+" \" 2 .\" for Sort by Id "+"\n"+" \" 3 .\" for Sort by Salary "+"\n"+" \" 4 .\" for Sort by Department ");
+        Scanner sc = new Scanner(System.in);
+        int o = sc.nextInt();
+        switch (o) {
+            case 1:
+                Set <Employee> ns = new TreeSet<Employee>(new nameComparator());
+                ns.add(emp0);
+                ns.add(emp1);
+                ns.add(emp2);
+                System.out.println("name in Sorted manner");
+                for(Employee e: ns ){
+                    System.out.println(e);
+                }
+                break;
+            case 2:
+                System.out.println("Id in sorted manner");
+                Set <Employee> Ids = new TreeSet<Employee>(new idComparator());
+                Ids.add(emp0);
+                Ids.add(emp1);
+                Ids.add(emp2);
+                for(Employee e: Ids ){
+                    System.out.println(e);
+                }
+                break;
+            case 3:
+                System.out.println("Salary in sorted manner");
+                Set <Employee>  sS = new TreeSet<Employee>(new idComparator());
+                sS.add(emp0);
+                sS.add(emp1);
+                sS.add(emp2);
+                for(Employee e: sS ){
+                    System.out.println(e);
+                }
+                break;
+            case 4:
+                System.out.println("Department in sorted manner");
+                Set <Employee>  dS = new TreeSet<Employee>(new idComparator());
+                dS.add(emp0);
+                dS.add(emp1);
+                dS.add(emp2);
+                for(Employee e: dS ){
+                    System.out.println(e);
+                }
+                break;
+
         }
 
     }
+
+
 }
+
+
