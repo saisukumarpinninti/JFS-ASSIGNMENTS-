b1ad602a3c63b088e6c13df59a94ef9219cdef62
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-04-02 22:16:20 +0530

    Updated Js Assignments

---
+++ JavaScript/Functional-Programming/ReadMe.md
@@ -0,0 +1,13 @@
+## Hello👋!
+
+
+ This Folder contains my Submission files for  **JFS-Angular Feb2022/JS ASSIGNMENTS**
+
+## Structure of the Folder
+
+> - index.js Contains Solutions For Questions of Functional Programming: Basic Exercises 1 to 4  refer to pdf page No 1.
+> - Please Refer To the Functional-Programming pdf for the Assignment Details .
+
+## Thanks for Visiting 😊
+
+### End
---
+++ JavaScript/Functional-Programming/js/index.js
@@ -0,0 +1,26 @@
+const double= function(x) { return x * 2;};
+const square = x => x * x;
+
+function composedValue(f1,f2,f){
+    return(f1(f2(f)));
+}
+function Q1(){
+    alert(composedValue(square,double,parseInt(prompt ('enter the Number'))));
+}
+
+function Q2(form){
+   var m1 = form.m1.value;
+    var m2 = form.m2.value;
+    var n = parseInt(form.f.value);
+    if(m1=='square' && m2=='double'){
+        var result = composedValue(square,double,n);}
+    else if(m1=='square' && m2=='square'){
+        var result = composedValue(square,square,n);}
+    else if(m1=='double' && m2=='double'){
+        var result = composedValue(double,double,n);}
+    else if(m1=='double' && m2=='square'){
+        var result = composedValue(double,square,n);}
+    else 
+        alert('invalid input');
+     document.getElementById('hello').innerHTML = `The result of composedvalue of ${m1}(${m2}(${n})) is ` + result;
+}
