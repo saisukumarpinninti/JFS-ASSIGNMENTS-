e5225c2cfea1c83582f53b3bdf7cce4d9a01750f
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-04-02 22:29:57 +0530

    Updated Js Assignments

--- JavaScript/Functional-Programming/js/index.js
+++ JavaScript/Functional-Programming/js/index.js
@@ -1,5 +1,7 @@
 const double= function(x) { return x * 2;};
 const square = x => x * x;
+const isEven = x => x % 2 === 0;
+var arr = [9, 3, 5, 4, 2];
 
 function composedValue(f1,f2,f){
     return(f1(f2(f)));
@@ -7,7 +9,6 @@ function composedValue(f1,f2,f){
 function Q1(){
     alert(composedValue(square,double,parseInt(prompt ('enter the Number'))));
 }
-
 function Q2(form){
    var m1 = form.m1.value;
     var m2 = form.m2.value;
@@ -23,4 +24,15 @@ function Q2(form){
     else 
         alert('invalid input');
      document.getElementById('hello').innerHTML = `The result of composedvalue of ${m1}(${m2}(${n})) is ` + result;
+}
+function Q3(){
+    var i = 0;
+    while(i<arr.length){
+        if(isEven(arr[i])){
+            var even = arr[i];
+            break;
+        }
+        i++;
+    }
+    document.getElementById('hello').innerHTML = `The even number is ${even}`;
 }
