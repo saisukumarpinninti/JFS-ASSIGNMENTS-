3525ffec5397f47fb3ac3cae993618d74ea9992a
saisukumarpinninti <saisukumarpinniti@gmail.com>
2022-04-05 16:56:21 +0530

    Added Es6 & ts Assignment-02

---
+++ Es6&Ts/Assignment02/js/demo.js
@@ -0,0 +1,108 @@
+//Assignment 02  :
+//Q1 :
+console.log("Q1");
+class Fibonacci {
+    constructor() {
+        this.previousNo = Symbol();
+        this.currentNo = Symbol();
+        this.previousNo = 0;
+        this.currentNo = 1;
+    }
+    next() {
+        let nxt = Symbol();
+        nxt = this.previousNo + this.currentNo;
+        this.previousNo = this.currentNo;
+        this.currentNo = nxt;
+        return nxt;
+    }
+}
+let obj = new Fibonacci();
+// console.log(obj);
+console.log("Fibonacci series End");
+console.log(obj.next()); // 1
+console.log(obj.next()); // 2
+console.log(obj.next()); // 3
+console.log(obj.next()); // 5
+console.log("Q2");
+//Q2 : 
+const listArmStrong = [0, 1, 153, 370, 371, 407];
+let n = 0;
+function Q2() {
+    return { getNextArmstrong: function () { n += 1; return { value: listArmStrong[n], done: false }; } };
+}
+// Create Iterable
+var n1 = Q2();
+console.log("Armstrong numbers are : ");
+console.log(n1.getNextArmstrong());
+console.log(n1.getNextArmstrong());
+console.log(n1.getNextArmstrong());
+console.log(n1.getNextArmstrong());
+console.log(n1.getNextArmstrong());
+//Q3 :
+console.log("Q3");
+//code to check a number is armstrong or not
+function isArmstrong(number) {
+    let sum = 0;
+    let temp = number;
+    while (temp > 0) {
+        let digit = temp % 10;
+        sum += digit ** 3;
+        temp = Math.floor(temp / 10);
+    }
+    return (number == sum);
+}
+//Generator function tocall isArmstrong and reset the value of n if n is greater than 1000
+function* getNextArmstrong1() {
+    let i = 0;
+    while (true) {
+        if (isArmstrong(i)) {
+            yield i;
+        }
+        if (i > 1000) {
+            i = 0;
+            yield "number is too larger";
+        }
+        i++;
+    }
+}
+console.log("Armstrong numbers are : ");
+console.log("using Generator");
+var g = getNextArmstrong1();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value); // number is too larger 
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+//Q4 :
+//using set to store users
+var croom1 = new Map();
+var croom2 = new Map();
+// adding users and messages
+croom1.set("user1", ["Hello", "How are you?", "I am fine", "Thank you"]);
+croom1.set("user2", ["Hi", "How are you?", "I am fine", "Thank you"]);
+croom1.set("user3", ["Hello", "How are you?", "I am fine", "Thank you"]);
+croom2.set("user4", ["Hello", "How are you?", "I am fine", "Thank you"]);
+croom2.set("user5", ["Hello", "How are you?", "I am fine", "Thank you"]);
+croom2.set("user6", ["Hello", "How are you?", "I am fine", "Thank you"]);
+//printing users and messages
+var printusers = (room, name) => {
+    console.log(`Users in room name  ${name} are : `);
+    for (let key of room.keys()) {
+        console.log(key);
+    }
+};
+var printmessages = (room, name) => {
+    console.log(`Messages in room name  ${name} are : `);
+    for (let value of room.values()) {
+        console.log(value);
+    }
+};
+printusers(croom1, "chatroom1");
+printusers(croom2, "chatroom2");
+printmessages(croom1, "chatroom1");
+printmessages(croom2, "chatroom2");
---
+++ Es6&Ts/Assignment02/readme.md
@@ -0,0 +1,22 @@
+
+### The file demo.ts contains code that is asked to solve in the assignments
+
+> * [Assignment 2](./demo.ts)
+
+## The Questions are
+
+<ol>
+<b>Symbols:</b>
+<li>  Write a class that defines next() method to return next number from Fibonacci series. The class will have a private attributes ‘previousNo’ & ‘currentNo’.</li>
+<li> 
+ Write a program that returns next Armstrong number after calling getNextArmstrong() method.</li>
+<br>
+<b>Generators:</b>
+<li>
+ Write a program that returns next Armstrong number after calling getNextArmstrong() method. Add functionality to reset generating Armstrong number from zero. In case, Armstrong number goes above one thousand then throw an error.</li>
+<br>
+<b>Collections:</b>
+
+<li>
+ Using Set & Map, create a static data for chatting application. Here we have 2 chatrooms, every chatroom is having 3 users &
+every user has posted different messages in a chat room. Note that one user can belong to a single chat room only. Now you need to find out how you will hold this data using Set & Map data structures. Also add functionality to get list of all users from a specific chatroom & listing down all message from a chatroom.
